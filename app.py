02fcc681e39cdfba363e6a3a4fbc0494528ad329
lilian fadi abusalha <267268225+lilianfadi3@users.noreply.github.com>
2026-04-13 15:20:57 +0300

    Change default db_host from 'localhost' to 'postgres'

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ db_uri = os.getenv("POSTGRES_URI") or os.getenv("DATABASE_URL")
 if not db_uri:
     db_user = os.getenv("POSTGRES_USER", "postgres")
     db_password = os.getenv("POSTGRES_PASSWORD", "postgres")
-    db_host = os.getenv("POSTGRES_HOST", "localhost")
+    db_host = os.getenv("POSTGRES_HOST", "postgres")
     db_port = os.getenv("POSTGRES_PORT", "5432")
     db_name = os.getenv("POSTGRES_DB", "taskdb")
     db_uri = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
